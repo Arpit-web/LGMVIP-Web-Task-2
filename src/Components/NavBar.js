31c19f1539da4968f4f15c88019384740b435fa4
Arpit-web <arpitwebdev188@gmail.com>
2023-07-16 01:15:36 +0530

    Add UserCard, UserGrid, Loader components and update App.js

---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = ({ getUsers }) => {
+  return (
+    <nav>
+      <div className="brand">Jenkins©</div>
+      <button onClick={getUsers}>Get Users</button>
+    </nav>
+  );
+};
+
+export default NavBar;
