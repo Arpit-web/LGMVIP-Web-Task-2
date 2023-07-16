55a57796d5730884545e1fcaec5254144dd54567
Arpit-web <arpitwebdev188@gmail.com>
2023-07-17 01:55:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Navbar from './Components/NavBar';
+import UserGrid from './Components/UserGrid';
+import Loader from './Components/Loader';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getUsers = async () => {
+    setIsLoading(true);
+    await new Promise(resolve => setTimeout(resolve, 1500));
+
+    try {
+      const response = await axios.get('https://reqres.in/api/users?page=1');
+      setUsers(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar getUsers={getUsers} />
+      {isLoading ? <Loader /> : <UserGrid users={users} />}
     </div>
   );
-}
+};
 
 export default App;
